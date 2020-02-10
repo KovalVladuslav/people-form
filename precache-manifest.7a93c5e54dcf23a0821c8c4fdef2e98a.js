ce614b679f01b79b94f101cc133b56bb5111bfb4
boom40 <kickgames675@gmail.com>
2020-02-10 22:42:57 +0200

    Updates

---
+++ precache-manifest.7a93c5e54dcf23a0821c8c4fdef2e98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3ac36ca9ec396ef11912d08de653ce",
+    "url": "/Softindex-test/index.html"
+  },
+  {
+    "revision": "70efe82de50aa523f3c3",
+    "url": "/Softindex-test/static/css/main.f2ec7acf.chunk.css"
+  },
+  {
+    "revision": "d5fb280a9cccb76849a7",
+    "url": "/Softindex-test/static/js/2.ff249455.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/Softindex-test/static/js/2.ff249455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70efe82de50aa523f3c3",
+    "url": "/Softindex-test/static/js/main.bcb73fdc.chunk.js"
+  },
+  {
+    "revision": "5b9511c5071d329745bc",
+    "url": "/Softindex-test/static/js/runtime-main.22ac3200.js"
+  }
+]);
